992a6b8278acd595e09029e697b1a52f4a28f3ab
JFKDevelopers <jfkdevelopers@gmail.com>
2016-10-20 15:45:21 -0400

    Update Defibrillators.java

--- Puzzles/Easy/Defibrillators.java
+++ Puzzles/Easy/Defibrillators.java
@@ -17,23 +17,12 @@ These fields are separated by a semicolon (;).
 
 Beware: the decimal numbers use the comma (,) as decimal separator. Remember to turn the comma (,) into dot (.) if necessary in order to use the data in your program.
  
-DISTANCE
-The distance d between two points A and B will be calculated using the following formula:
-
-
-​
-Note: In this formula, the latitudes and longitudes are expressed in radians. 6371 corresponds to the radius of the earth in km.
-
 The program will display the name of the defibrillator located the closest to the user’s position. This position is given as input to the program.
- 	Game Input
-
+ 
 Input
 Line 1: User's longitude (in degrees)
-
 Line 2: User's latitude (in degrees)
-
 Line 3: The number N of defibrillators located in the streets of Montpellier
-
 N next lines: a description of each defibrillator
 
 Output
@@ -43,17 +32,19 @@ Constraints
 0 < N < 10000
 
 Example
-Entrée
+Input:
 3,879483
 43,608177
 3
 1;Maison de la Prevention Sante;6 rue Maguelone 340000 Montpellier;;3,87952263361082;43,6071285339217
 2;Hotel de Ville;1 place Georges Freche 34267 Montpellier;;3,89652239197876;43,5987299452849
 3;Zoo de Lunaret;50 avenue Agropolis 34090 Mtp;;3,87388031141133;43,6395872778854
-Sortie
+
+Output:
 Maison de la Prevention San
 */
 
+//Solution by JFKDevelopers
 import java.util.*;
 import java.io.*;
 import java.math.*;
